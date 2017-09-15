af0111b6aa0cc8af7e7f1071f11933a52bf66fd2
Federico Canet <fedeea@gmail.com>
2017-09-15 20:58:48 -0300

    Manhattan and hamming distance implemented

--- src/Viernes-15-09.hs
+++ src/Viernes-15-09.hs
@@ -1,3 +1,6 @@
+import Data.Char
+import Data.List
+
 isDigitStr :: String -> Bool
 isDigitStr str = foldr(&&) True (map isDigit str)
 
@@ -22,3 +25,10 @@ isSorted :: (Ord a) => [a] -> Bool
 isSorted (a:b:xs) = a <= b && isSorted(b:xs)
 isSorted [a] = True
 isSorted [] = True
+
+manhattanDistance v1 v2
+ | length v1 /= length v2 = error "No!"
+ | otherwise = sum (zipWith (\x y -> abs(x-y)) v1 v2)
+
+hammingDistance :: (Eq a) => [a] -> [a] -> Int
+hammingDistance xs ys = sum(zipWith (\x y -> if x == y then 0 else 1) xs ys) + abs(length xs - length ys)
