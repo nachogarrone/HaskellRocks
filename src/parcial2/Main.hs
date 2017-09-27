fcf6e67859afa7fd0144b5e9c246f4e83fe3b3ca
Nacho Garrone <nachogarrone@gmail.com>
2017-09-27 19:59:45 -0300

    Merge

--- src/parcial2/Main.hs
+++ src/parcial2/Main.hs
@@ -7,7 +7,7 @@ main = do
 --    print (monthDays Feb 2017)
 --    print (monthDays Feb 2016)
 --    print (monthDays Feb 2015)
-   print(walk (0,0) [StepLeft, StepUp, StepLeft])
+    print(walk (0,0) [StepLeft, StepUp, StepLeft])
     print (gameRPS Papel Tijera)
 --     print (gameRPS (Papel, Piedra) (Papel, Papel))
 --     print (gameRPS (Papel, Piedra) (Papel, Tijera))
