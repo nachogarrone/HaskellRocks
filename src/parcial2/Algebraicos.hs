9f4323dd32ad99054821e1cb6472697580247e11
Nacho Garrone <nachogarrone@gmail.com>
2017-09-27 19:58:41 -0300

    RPS

--- src/parcial2/Algebraicos.hs
+++ src/parcial2/Algebraicos.hs
@@ -5,6 +5,8 @@ data Month = Ene | Feb | Mar | Abr | May | Jun | Jul | Ago | Sep | Oct | Nov | D
 
 -- data Step = StepLeft | StepLeft | StepLeft | StepLeft deriving (Eq, Show)
 
+data RPS = Piedra | Papel | Tijera deriving (Eq, Show)
+
 -- monthDays:: Month -> Int -> Int
 monthDays Feb y = if (esBisiesto y) then 29 else 28
 monthDays Ene _ = 31
@@ -21,3 +23,17 @@ monthDays Dic _ = 31
 
 esBisiesto::Int->Bool
 esBisiesto x = ( mod x 400==0) || (mod x 4==0) && not (mod x 100==0)
+
+
+-- gameRPS2 (ax,az) (bx,bz) = compare (compareRPS ax az) (compareRPS bx bz)
+gameRPS a b = (compareRPS a b)
+
+compareRPS Piedra Tijera = 1
+compareRPS Piedra Papel = -1
+compareRPS Piedra Piedra = 0
+compareRPS Tijera Piedra = -1
+compareRPS Tijera Papel = 1
+compareRPS Tijera Tijera = 0
+compareRPS Papel Tijera = -1
+compareRPS Papel Piedra = 1
+compareRPS Papel Papel = 0
--- src/parcial2/Main.hs
+++ src/parcial2/Main.hs
@@ -4,6 +4,10 @@ import Algebraicos
 
 main :: IO ()
 main = do
-    print (monthDays Feb 2017)
-    print (monthDays Feb 2016)
-    print (monthDays Feb 2015)
+    print (gameRPS Papel Tijera)
+--     print (gameRPS (Papel, Piedra) (Papel, Papel))
+--     print (gameRPS (Papel, Piedra) (Papel, Tijera))
+--     print (gameRPS (Papel, Tijera) (Papel, Tijera))
+--     print (monthDays Feb 2017)
+--     print (monthDays Feb 2016)
+--     print (monthDays Feb 2015)
