bf22acc5634efd9ecc7e28410dd128ed77422917
Federico Canet <fedeea@gmail.com>
2017-09-27 19:51:26 -0300

    Implementado walk

--- src/parcial2/Algebraicos.hs
+++ src/parcial2/Algebraicos.hs
@@ -3,7 +3,7 @@ module Algebraicos where
 
 data Month = Ene | Feb | Mar | Abr | May | Jun | Jul | Ago | Sep | Oct | Nov | Dic deriving (Eq, Show)
 
--- data Step = StepLeft | StepLeft | StepLeft | StepLeft deriving (Eq, Show)
+data Step = StepLeft | StepRight | StepUp | StepDown deriving (Eq, Show)
 
 -- monthDays:: Month -> Int -> Int
 monthDays Feb y = if (esBisiesto y) then 29 else 28
@@ -21,3 +21,11 @@ monthDays Dic _ = 31
 
 esBisiesto::Int->Bool
 esBisiesto x = ( mod x 400==0) || (mod x 4==0) && not (mod x 100==0)
+
+walk :: (Int, Int) -> [Step] -> (Int, Int)
+walk a [] = a
+walk (a,b) (x:xs)
+    | x == StepLeft = walk (a-1, b) xs
+    | x == StepRight = walk (a+1, b) xs
+    | x == StepUp = walk (a, b+1) xs
+    | x == StepDown = walk (a, b-1) xs
--- src/parcial2/Main.hs
+++ src/parcial2/Main.hs
@@ -4,6 +4,7 @@ import Algebraicos
 
 main :: IO ()
 main = do
-    print (monthDays Feb 2017)
-    print (monthDays Feb 2016)
-    print (monthDays Feb 2015)
+--    print (monthDays Feb 2017)
+--    print (monthDays Feb 2016)
+--    print (monthDays Feb 2015)
+   print(walk (0,0) [StepLeft, StepUp, StepLeft])
