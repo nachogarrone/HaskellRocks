d91622572d310ea79da2f11cb93d76790cdd3a06
Nacho Garrone <nachogarrone@gmail.com>
2017-09-29 21:01:46 -0300

    JuegoCartas

---
+++ src/parcial2/JuegoCartas.hs
@@ -0,0 +1,65 @@
+module JuegoCartas where
+
+import Data.List
+
+data Naipe = Oros Int | Copas Int | Espadas Int | Bastos Int deriving (Eq, Show)
+data FileSystem = Folder String [FileSystem] | File String Int deriving (Eq, Show)
+data BinTree a = Node a (BinTree a) (BinTree a) | Leaf a | Empty deriving (Eq, Show)
+
+suitNaipe (Oros _) = 1
+suitNaipe (Copas _) = 2
+suitNaipe (Espadas _) = 3
+suitNaipe (Bastos _) = 4
+
+
+numberNaipe (Oros n) = n
+numberNaipe (Copas n) = n
+numberNaipe (Espadas n) = n
+numberNaipe (Bastos n) = n
+
+
+baraja :: [Naipe]
+baraja = [s n | s <- [Oros, Copas, Espadas, Bastos], n <- [1..12]]
+
+
+turnoEscopa15 :: [Naipe] -> Naipe -> [Naipe]
+turnoEscopa15 ns j
+    | null combs = j:ns
+    | otherwise = ns \\ concat combs
+    where combs = [c | c <- powerest ns, sum (map numberNaipe (j:c)) == 15]
+
+
+powerest [] = [[]]
+powerest (x:xs) = p ++ (map (x:) p)
+    where p = powerest xs
+
+
+size (Folder _ []) = 0
+size (File _ n) = n
+size (Folder _ x) = sum (map size x)
+
+
+preorder (Leaf n) = [n]
+preorder (Empty) = []
+preorder (Node n b1 b2) = [n] ++ (preorder b1) ++ (preorder b2)
+
+inorder (Leaf n) = [n]
+inorder (Empty) = []
+inorder (Node n b1 b2) = (inorder b1) ++ [n] ++ (inorder b2)
+
+postorder (Leaf n) = [n]
+postorder (Empty) = []
+postorder (Node n b1 b2) = (postorder b1) ++ (postorder b2) ++ [n]
+
+binTreeSearch tree n = elem n (preorder tree)
+
+isOrderedTree tree = ascending (inorder tree)
+
+ascending [] = True
+ascending [x] = True
+ascending (x:y:xs) = x <= y && ascending (y:xs)
+
+
+replaceBinTree Empty _ _ = Empty
+replaceBinTree (Leaf n) v new = if (n == v) then new else Leaf n
+replaceBinTree (Node n b1 b2) v new = if (n==v) then new else (if n<v then Node n (replaceBinTree b1 v new) b2 else Node n b1 (replaceBinTree b2 v new))
--- src/parcial2/Main.hs
+++ src/parcial2/Main.hs
@@ -1,22 +1,38 @@
 module Main where
 
-import Algebraicos
+import JuegoCartas
+
+
 
 main :: IO ()
 main = do
-    print (toRGB GREEN)
-    print (toRGB WHITE)
-    print (toRGB (RGB 1 2 3))
+    print (replaceBinTree (Leaf 2) 3 Empty)
+    print (replaceBinTree (Leaf 2) 2 Empty)
+--     print (isOrderedTree (Node 2 (Leaf 1) (Leaf 3)))
+--     print (isOrderedTree (Node 1 (Leaf 2) (Leaf 3)))
+--     print (binTreeSearch (Node 2 (Leaf 1) (Leaf 3)) 1)
+--     print (binTreeSearch (Node 2 (Leaf 1) (Leaf 3)) 4)
+--     print (preorder (Node 7 (Leaf 3) (Node 9 Empty Empty)))
+--     print (inorder (Node 7 (Leaf 3) (Leaf 9)))
+--     print (postorder (Node 7 (Leaf 3) (Leaf 9)))
+--     print (size (Folder "a" []))
+--     print (size (File "a.txt" 120))
+--     print (size (Folder "a" [Folder "b" []]))
+--     print (size (Folder "a" [File "b.txt" 20]))
+--     print (turnoEscopa15 [(Oros 7), (Bastos 1), (Bastos 3)] (Espadas 3))
+--     print (toRGB GREEN)
+--     print (toRGB WHITE)
+--     print (toRGB (RGB 1 2 3))
 --     print (toInches (Feet 1.0))
 --     print (toFeet (Yards 1.0))
 --     print (toYards (Inches 72.36))
 --     print (toInches (Inches 2.34))
 --     print (toFeet (Inches (-12.0)))
 --     print (toYards (Feet 0.0))
-    print(addNum (IntNum 12) (IntNum 34))
-    print(addNum (DblNum 1.2) (DblNum 3.4))
-    print(addNum (DblNum 1.2) (IntNum 34))
-    print(addNum (IntNum 12) (DblNum 3.4))
+--     print(addNum (IntNum 12) (IntNum 34))
+--     print(addNum (DblNum 1.2) (DblNum 3.4))
+--     print(addNum (DblNum 1.2) (IntNum 34))
+--     print(addNum (IntNum 12) (DblNum 3.4))
 --     print (toInches (Feet 1.0))
 --     print (toFeet (Yards 1.0))
 --     print (toYards (Inches 72.36))
