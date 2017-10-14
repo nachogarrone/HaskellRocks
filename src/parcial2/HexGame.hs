a387ec68933aa426cbaa3335d41fb1d3bb363b87
Federico Canet <fedeea@gmail.com>
2017-10-13 21:02:49 -0300

    cambios

--- src/parcial2/HexGame.hs
+++ src/parcial2/HexGame.hs
@@ -13,7 +13,7 @@ type HexMove = (Int, Int)
 -----------------------------------------------------------
 
 activePlayer :: HexBoard -> Maybe HexPlayer
-activePlayer board = if (mod (sum (map length (map filter (== 'e') board))) 2 == 0) then 'W' else 'B'
+activePlayer board = if (mod (sum (map length (map (filter (/= 'e')) board))) 2 == 0) then (Just 'W') else (Just 'B')
 
 -- filtramos las n
 -- aplicamos map para obtener todas las listas de fitler
@@ -22,35 +22,35 @@ activePlayer board = if (mod (sum (map length (map filter (== 'e') board))) 2 ==
 
 -----------------------------------------------------------
 
-isFinished :: HexBoard -> Bool
-isFinished board = (winner board)
+--isFinished :: HexBoard -> Bool
+--isFinished board = (winner board)
 
 
 -----------------------------------------------------------
 
 moves :: HexBoard -> HexPlayer -> [HexMove]
 moves board player
-    | player /= (activePlayer board) = []
-    | otherwise = [(x,y) | x <- 0...10, y <- 0...10, (board !! x) !! y == 'e']
+    | player /= fromJust(activePlayer board) = []
+    | otherwise = [(x,y) | x <- [0..10], y <- [0..10], (board !! x) !! y == 'e']
 
 -----------------------------------------------------------
 
-nextBoard :: HexBoard -> HexPlayer -> HexMove -> HexBoard
+--nextBoard :: HexBoard -> HexPlayer -> HexMove -> HexBoard
 
 
 -----------------------------------------------------------
 
-winner :: HexBoard -> Maybe HexPlayer
+--winner :: HexBoard -> Maybe HexPlayer
 
 
 -----------------------------------------------------------
 
 startBoard :: HexBoard
-startBoard = [["eeeeeeeeeee"],["eeeeeeeeeee"],["eeeeeeeeeee"],["eeeeeeeeeee"],["eeeeeeeeeee"],["eeeeeeeeeee"],["eeeeeeeeeee"],["eeeeeeeeeee"],["eeeeeeeeeee"],["eeeeeeeeeee"],["eeeeeeeeeee"]]
+startBoard = ["eeeeeeeeeee","eeeeeeeeeee","eeeeeeeeeee","eeeeeeeeeee","eeeeeeeeeee","eeeeeeeeeee","eeeeeeeeeee","eeeeeeeeeee","eeeeeeeeeee","eeeeeeeeeee","eeeeeeeeeee"]
 
 -----------------------------------------------------------
 
-toString :: HexBoard -> String
+--toString :: HexBoard -> String
 
 
 -----------------------------------------------------------
