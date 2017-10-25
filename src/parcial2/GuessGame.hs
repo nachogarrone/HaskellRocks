0937ae5e11890e62309d3f9dd4d3c81e0ea0d0e0
Nacho Garrone <nachogarrone@gmail.com>
2017-10-25 20:57:48 -0300

    GuessGame WIP

--- src/parcial2/GuessGame.hs
+++ src/parcial2/GuessGame.hs
@@ -1,8 +1,17 @@
 module GuessGame where
 
+import Data.List
 import System.Random
+import Data.IORef
 
 type Player = Maybe [Int] -> IO String
+
+bannedChars = newIORef ()
+
+-- randomPlayer :: Int -> String -> Player
+-- randomPlayer n s _ = randomStr s n
+
+-- smartPlayer
 
 consolePlayer Nothing = getLine
 consolePlayer (Just chk) = do
@@ -13,14 +22,20 @@ guessGame len chars tries player = do
    str <- randomStr len chars
    _guessGame str tries Nothing player
 
-
-_guessGame :: String -> Int -> Maybe [Int] -> Player
-   -> IO Int
-_guessGame s t x p =
-   if t <= 0 || x == Just (take (length s) (repeat 2))
-   then return (max 0 t) else do
+_guessGame :: String -> Int -> Maybe [Int] -> Player -> IO Int
+_guessGame s t x p = if t <= 0 || x == Just (take (length s) (repeat 2))
+    then return (max 0 t)
+    else do
       m <- p x
       _guessGame s (t - 1) (Just (checkStr s m)) p
+
+
+-- compareList _ [] = return []
+-- compareList (s:string) (r:results) = do
+--     if r == 0 then do
+--         writeIORef bannedChars s else print "no sacar nada"
+--     compareList string results
+
 
 
 checkStr :: String -> String -> [Int]
