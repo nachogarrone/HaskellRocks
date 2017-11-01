565a54e324f5af35eedba0b127ad56f983f89a8e
Federico Canet <fedeea@gmail.com>
2017-11-01 20:05:14 -0300

    substitution

--- src/parcial2/Lambda.hs
+++ src/parcial2/Lambda.hs
@@ -13,3 +13,16 @@ freevars :: Lambda -> [Int]
 freevars (Var x) = [x]
 freevars (Apl e f) = union (freevars e) (freevars f)
 freevars (Abs x e) = delete x (freevars e)
+
+substitution :: Lambda -> Int -> Lambda -> Lambda
+
+substitution e@(Var x) y r
+    | y == x = r
+    | otherwise = e
+
+substitution (Apl e f) x r =
+    (Apl (substitution e x r) (substitution f x r))
+
+substitution e@(Abs x f) y r
+    | x == y = e
+    | otherwise = (Abs x (substitution f y r))
